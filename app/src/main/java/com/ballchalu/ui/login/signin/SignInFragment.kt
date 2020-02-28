fd861c7e5feed3dc3caaabfcd38073d142bc6427
dl023 <soham.bagade@definelabs.com>
2020-02-28 12:23:24 +0530

    google sign in done

--- app/src/main/java/com/ballchalu/ui/login/signin/SignInFragment.kt
+++ app/src/main/java/com/ballchalu/ui/login/signin/SignInFragment.kt
@@ -1,19 +1,31 @@
 package com.ballchalu.ui.login.signin
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModelProvider
 import com.ballchalu.base.BaseFragment
 import com.ballchalu.databinding.FragmentSignInBinding
+import com.ballchalu.utils.AppConstants
 import com.ccpp.shared.core.result.EventObserver
 import com.ccpp.shared.util.viewModelProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.tasks.Task
+import timber.log.Timber
 import javax.inject.Inject
 
-class SignInFragment : BaseFragment() {
+
+class SignInFragment : BaseFragment(), GoogleApiClient.OnConnectionFailedListener {
+    private var mGoogleSignInClient: GoogleSignInClient? = null
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
@@ -29,14 +41,15 @@ class SignInFragment : BaseFragment() {
             lifecycleOwner = this@SignInFragment
         }
 
-
-
+        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
 
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        initGoogleAndFB()
+
         viewModel.loginFormState.observe(viewLifecycleOwner, EventObserver {
             val loginState = it ?: return@EventObserver
 
@@ -75,13 +88,51 @@ class SignInFragment : BaseFragment() {
             )
         }
 
+        binding.llGoogleSignIn.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, AppConstants.GOOGLE_SIGN_IN_REQUEST)
+    }
+
+    private fun initGoogleAndFB() {
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
     }
 
     private fun updateUiWithUser(status: String?) {
         Toast.makeText(requireContext(), "$status", Toast.LENGTH_LONG).show()
     }
 
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(requireContext(), errorString, Toast.LENGTH_SHORT).show()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AppConstants.GOOGLE_SIGN_IN_REQUEST) {
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+            updateUiWithUser(account?.email)
+        } catch (e: ApiException) { // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Timber.d("signInResult:failed code=%s", e.statusCode)
+            updateUiWithUser("Something went wrong " + e.message)
+        }
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
     }
 }
--- app/src/main/java/com/ballchalu/utils/AppConstants.kt
+++ app/src/main/java/com/ballchalu/utils/AppConstants.kt
@@ -2,5 +2,6 @@ package com.ballchalu.utils
 
 object AppConstants {
 
+    const val GOOGLE_SIGN_IN_REQUEST = 1010
 
 }
