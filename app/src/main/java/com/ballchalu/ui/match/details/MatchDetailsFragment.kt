48d77ae3f89416f82f54e877850a296102287963
dl023 <soham.bagade@definelabs.com>
2020-02-29 17:58:38 +0530

    match details

---
+++ app/src/main/java/com/ballchalu/ui/match/details/MatchDetailsFragment.kt
@@ -0,0 +1,36 @@
+package com.ballchalu.ui.match.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.ballchalu.base.BaseFragment
+import com.ballchalu.databinding.FragmentMatchDetailsBinding
+import com.ccpp.shared.util.viewModelProvider
+import javax.inject.Inject
+
+class MatchDetailsFragment : BaseFragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var binding: FragmentMatchDetailsBinding
+    private lateinit var viewModel: MatchDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = viewModelProvider(viewModelFactory)
+        binding = FragmentMatchDetailsBinding.inflate(inflater).apply {
+            lifecycleOwner = this@MatchDetailsFragment
+        }
+
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/ballchalu/ui/match/details/MatchDetailsViewModel.kt
@@ -0,0 +1,8 @@
+package com.ballchalu.ui.match.details
+
+import com.ballchalu.base.BaseViewModel
+import javax.inject.Inject
+
+class MatchDetailsViewModel @Inject constructor() :
+    BaseViewModel() {
+}
